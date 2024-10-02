c52eade729b771a6be1f3b86b7f1fa89eabeccc5
Shilpa Sonnar <shilpasonnar1708@gmail.com>
2024-10-02 16:58:25 +0530

    Adding Reverse Name

---
+++ scrapDemo/src/com/scrap/Demo/ReverseName.java
@@ -0,0 +1,15 @@
+package com.scrap.Demo;
+
+public class ReverseName {
+
+	public static void main(String[] args) {
+		String str ="Ram ";
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			char ch = str.charAt(i);
+			System.out.print(ch);
+
+		}
+
+	}
+	}
